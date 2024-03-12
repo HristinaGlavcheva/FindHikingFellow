03b627734547f3070737f5240147f6fa8bf42a1c
Hristina Glavcheva <glavchevahristina@gmail.com>
2024-03-12 16:53:43 +0200

    Create database models

---
+++ FindHikingFellow.Infrastructure/Data/Models/Enumerations/Activity.cs
@@ -0,0 +1,16 @@
+﻿namespace FindHikingFellow.Infrastructure.Data.Models.Enumerations
+{
+    public enum Activity
+    {
+        Hiking = 1,
+        MountainBiking = 2,
+        Running = 3,
+        Backpacking = 4,
+        Walking = 5,
+        RoadBiking = 6,
+        RockClimbing = 7,
+        ViaFerrata = 8,
+        SnowShoeing =9,
+        Skiing = 10
+    }
+}
