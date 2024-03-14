57690dc24f5457cdd9a0ff9011da8e28ac7f82df
Hristina Glavcheva <glavchevahristina@gmail.com>
2024-03-14 14:15:14 +0200

    Modify DbContext

--- FindHikingFellow.Infrastructure/Data/ApplicationDbContext.cs
+++ FindHikingFellow.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FindHikingFellow.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace FindHikingFellow.Infrastructure.Data
 {
@@ -8,6 +10,45 @@ namespace FindHikingFellow.Infrastructure.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Tour> Tours { get; set; } = null!;
+
+        public DbSet<Destination> Destinations { get; set; } = null!;
+
+        public DbSet<Feature> Features { get; set; } = null!;
+
+        public DbSet<FeedBack> FeedBacks { get; set; } = null!;
+
+        public DbSet<Image> Images { get; set; } = null!;
+
+        public DbSet<KeyPoint> KeyPoints { get; set; } = null!;
+
+        public DbSet<PersonalList> PersonalLists { get; set; } = null!;
+
+        public DbSet<TipsAndTricks> tipsAndTricks { get; set; } = null!;
+
+        public DbSet<TourFeature> TourFeatures { get; set; } = null!;
+
+        public DbSet<TourKeyPoint> TourKeyPoints { get; set; } = null!;
+
+        public DbSet<TourParticipant> TourParticipants { get; set; } = null!;
+
+        public DbSet<TourPersonalList> TourPersonalLists { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Disable cascade delete
+            var entityTypes = builder.Model.GetEntityTypes().ToList();
+
+            var foreignKeys = entityTypes
+                .SelectMany(e => e.GetForeignKeys().Where(f => f.DeleteBehavior == DeleteBehavior.Cascade));
+            foreach (var foreignKey in foreignKeys)
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
         }
     }
 }
