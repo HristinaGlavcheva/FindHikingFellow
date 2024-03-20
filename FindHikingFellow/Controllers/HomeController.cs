88c032ac16a973df7bedcc6700928f0886538dac
Hristina Glavcheva <glavchevahristina@gmail.com>
2024-03-20 15:52:48 +0200

    Add TourController, modify HomeController

--- FindHikingFellow/Controllers/HomeController.cs
+++ FindHikingFellow/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using FindHikingFellow.Models;
+﻿using FindHikingFellow.Core.Contracts;
+using FindHikingFellow.Core.Models;
+using FindHikingFellow.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +9,41 @@ namespace FindHikingFellow.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IDestinationService destinationService;
+        private readonly ITourService tourService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            IDestinationService _destinationsService,
+            ITourService _tourService)
         {
             _logger = logger;
+            destinationService = _destinationsService;
+            tourService = _tourService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var destinationServiceModels = await destinationService.GetMostPopularDestinationsAsync();
+            var tourServiceModels = await tourService.GetMostResentToursAsync();
+
+            var indexViewModel = new IndexViewModel
+            {
+                Destinations = destinationServiceModels.Select(d => new DestinationViewModel
+                {
+                    Name = d.Name,
+                    ImageUrl = d.ImageUrl,
+                })
+                .ToList(),
+                Tours = tourServiceModels.Select(t => new TourViewModel
+                {
+                    Name = t.Name,
+                    ImageUrl = t.ImageUrl,
+                })
+                .ToList(),
+            };
+
+            return this.View(indexViewModel);
         }
 
         public IActionResult Privacy()
